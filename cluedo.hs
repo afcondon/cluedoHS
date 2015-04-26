f8d416396a723025fe6d71ac73d98969a00c6713
Andrew Condon <andrew.condon@gmail.com>
2015-04-26 21:49:22 +0200

    refine types and improve makeSuggestion
    now produces the required input for learnFromSuggestion

--- cluedo.hs
+++ cluedo.hs
@@ -7,8 +7,6 @@ import Data.List
 import Data.List.Split
 import Data.Function (on)
 import System.Random
-import Control.Monad.Reader
-import Control.Monad.Writer
 import Control.Monad.State
 import Control.Lens
 import qualified Data.Map.Strict as Map
@@ -66,9 +64,25 @@ testSuggestion (s,w,p) h = or bools
         asList = [s,w,p]
         bools = map (flip elem asList) h
 
-makeSuggestion :: Suggestion -> [Hand] -> [Bool]
-makeSuggestion _ [] = []
-makeSuggestion s (h:hs) = testSuggestion s h : makeSuggestion s hs
+getPasses :: [(Bool, PlayerID)] -> [PlayerID]
+getPasses info = map snd $ takeWhile (not . fst) info
+
+getMatch :: [(Bool, PlayerID)] -> Maybe PlayerID
+getMatch [] = Nothing
+getMatch ((True, pid):_) = Just pid
+getMatch ((False, _):ps) = getMatch ps
+
+evalSuggestion :: Suggestion -> [Hand] -> [Bool]
+evalSuggestion _ [] = []
+evalSuggestion s (h:hs) = testSuggestion s h : evalSuggestion s hs
+
+makeSuggestion :: Suggestion -> [Hand] -> SuggestionResult
+makeSuggestion s hs = (passes, match)
+  where
+    results = zip (evalSuggestion s hs) [1..(length hs)]
+    passes = getPasses results
+    match  = getMatch results
+
 
 -- core information used for deduction and tracking
 -------------------------------------------------------------------------------
@@ -81,20 +95,7 @@ type PlayerCards = Map.Map PlayerID CardSet
 type PlayerCards' = Map.Map PlayerID CardSet
 type PlayerPassed = Map.Map PlayerID SuggestionSet
 type PlayerMatched = Map.Map PlayerID SuggestionSet
-
-data KB = KB { _cf  :: CardFacts
-             , _pc  :: PlayerCards   -- Map Player -> Set of Cards held
-             , _pcl :: Map.Map PlayerID [Card]
-             , _pc' :: PlayerCards' -- Map Player -> Set of Cards _not_ held
-             , _pm  :: PlayerMatched -- Map Player -> Set of Suggs Matched
-             , _pm' :: PlayerPassed -- Map Player -> Set of Suggs Passed
-             } deriving (Show, Eq)
-
-makeLenses ''KB
-
-
-addCardFact :: Card -> Ownership -> StateT KB
-addCardFact c o = cf.at c .= Just o
+type SuggestionResult = ([PlayerID], Maybe PlayerID)
 
 initCardFacts :: CardFacts
 initCardFacts = Map.fromList (zip allCards (repeat Unknown))
@@ -105,27 +106,40 @@ initCardSet = Set.empty
 initSuggestionSet :: SuggestionSet
 initSuggestionSet = Set.empty
 
+data KB = KB { _cf  :: CardFacts
+             , _pc  :: PlayerCards   -- Map Player -> Set of Cards held
+             , _pc' :: PlayerCards' -- Map Player -> Set of Cards _not_ held
+             , _pm  :: PlayerMatched -- Map Player -> Set of Suggs Matched
+             , _pm' :: PlayerPassed -- Map Player -> Set of Suggs Passed
+             } deriving (Show, Eq)
+
 initKB :: Int -> KB
 initKB n = KB { _cf  = initCardFacts
               , _pc  = Map.fromList $ zip [1..n] $ repeat initCardSet
-              , _pcl = Map.fromList $ zip [1..n] $ repeat []
               , _pc' = Map.fromList $ zip [1..n] $ repeat initCardSet
               , _pm  = Map.fromList $ zip [1..n] $ repeat initSuggestionSet
               , _pm' = Map.fromList $ zip [1..n] $ repeat initSuggestionSet }
 
-getPasses :: [(Bool, PlayerID)] -> [PlayerID]
-getPasses info = map snd $ takeWhile (not . fst) info
+makeLenses ''KB
 
-getMatch :: [(Bool, PlayerID)] -> Maybe PlayerID
-getMatch [] = Nothing
-getMatch ((True, pid):_) = Just pid
-getMatch ((False, _):ps) = getMatch ps
+addCardFact :: Card -> Ownership -> State KB ()
+addCardFact c o = cf.at c .= Just o
+
+addCardFacts :: CardFacts -> State KB ()
+addCardFacts cfs = cf %= (Map.union cfs)
+
 {-
-learnFromSuggestion :: KB -> Suggestion -> [PlayerID] -> Maybe PlayerID -> KB
-learnFromSuggestion kb (s,w,p) passes match = newKB
-    where
-        newKB = playerDoesNotHaveCard s (head passes)
+learnFromSuggestion :: Suggestion -> SuggestionResult -> State KB ()
+learnFromSuggestion sugg (ps,m) = pm' %= (Set.insert sugg)
+  where
+    fpm' = 
+
+reviewCardFacts :: State KB () 
+
+reviewSuggestions :: State KB ()
 -}
+
+
 ppKB :: KB -> IO ()
 ppKB kb = do
     putStrLn $ Pr.ppShow kb 
@@ -147,18 +161,6 @@ main = do
     let hs = deal remainingCards n
     let firstS = head suggestions
     let results = makeSuggestion firstS hs  -- not hs but hs-without-suggester
-    let info = zip results ps
-    let passes = getPasses info
-    let match = getMatch info 
-    print info
-    let kb' = kb^.cf & at MS .~ Just Noone
-    print kb'
-    let cfN = _cf kb
-    let csn = Map.lookup 1 (_pc kb)
-    print csn
-    let foo = case csn of Nothing -> initCardSet
-                          (Just c) -> Set.insert MS c
-    print foo
-    --print kb
+    print results
     -- learnFromSuggestion firstS passes match
     print "all done!"
