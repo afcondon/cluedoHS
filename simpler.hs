b06f31ed3f963c6e3e45008020e03cfe42735b3d
Andrew Condon <andrew.condon@gmail.com>
2015-04-26 21:48:39 +0200

    verify minimum libs for use case
    monoid not needed unless <>~ being used

--- simpler.hs
+++ simpler.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 import Control.Lens
-import Data.Monoid
+-- import Data.Monoid
 import Control.Monad.State
 import qualified Data.Map.Strict as Map
 
@@ -15,9 +15,10 @@ initDB =  DB { _myMap  = foo }
 
 add2Map :: Int -> Char -> State DB ()
 add2Map i c = myMap.at i .= (Just c)
-
+{-
 add2MapMultiple :: Map.Map Int Char -> State DB ()
 add2MapMultiple mmc = myMap <>= mmc
+-}
 
 
 add2MapMultiple2 :: Map.Map Int Char -> State DB ()
@@ -31,8 +32,8 @@ bar = Map.fromList (zip [4..9] ['n'..'z'])  :: Map.Map Int Char
 main :: IO ()
 main = do
   let ndb = execState (add2Map 1 'z') db
-  let ndb2 = execState (add2MapMultiple bar) ndb
+  -- let ndb2 = execState (add2MapMultiple bar) ndb
   let ndb3 = execState (add2MapMultiple2 bar) ndb
   print ndb
-  print ndb2
+  -- print ndb2
   print ndb3
