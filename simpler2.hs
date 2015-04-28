af65d70831f1520350cef55ac053240760b19fcc
Andrew Condon <andrew.condon@gmail.com>
2015-04-28 16:41:15 +0200

    tidied up for SO

--- simpler2.hs
+++ simpler2.hs
@@ -10,49 +10,43 @@ import qualified Data.Set as Set
 type CharSet = Set.Set Char
 type MapOfSets = Map.Map Int CharSet
 
-data DB = DB { _myMap  :: MapOfSets} deriving (Show, Eq)
+data DB = DB { _mos  :: MapOfSets } deriving (Show, Eq)
 
 makeLenses ''DB
 
 initDB :: DB
-initDB =  DB { _myMap  = Map.fromList (zip [1..5] (repeat Set.empty)) }
+initDB =  DB { _mos  = Map.fromList (zip [1..5] (repeat Set.empty)) }
 
 add2Map :: Int -> CharSet -> State DB ()
-add2Map i cs = myMap.ix i %= (Set.union cs)
+add2Map i cs = mos.ix i %= (Set.union cs)
 
 traverseAll :: Traversal' DB CharSet
-traverseAll = myMap.traversed
+traverseAll = mos.traversed
 
 add2MapsAll :: CharSet -> State DB ()
 add2MapsAll cs = traverseAll %= (Set.union cs)
 
+--        <problematic part>          
+{-
 traverseSome :: [Int] -> Int -> Traversal' DB MapOfSets
 traverseSome i is = _
 
 add2MapsSome :: [Int] -> CharSet -> State DB ()
-add2MapsSome is cs = traverseAll %= (Set.union cs)
-
-{-
-around :: Point -> Double -> Traversal' Unit Unit
-around center radius = 
-  filtered (\unit -> (unit^.position.x - center^.x)^2 + (unit^.position.y - center^.y)^2 < radius^2 )
-
-fireBreath :: Point -> StateT Game IO ()
-fireBreath target = do
-    lift $ putStrLn "*rawr*"
-    units.traversed.(around target 1.0).health -= 3
--}
-
-bar = Set.fromList ['a'..'g'] :: CharSet
-baz = Set.fromList ['f'..'m'] :: CharSet
-quux = Set.fromList ['n'..'z'] :: CharSet
+add2MapsSome is cs = (traverseSome is _) %= (Set.union cs)
+-}         
+--        </problematic part>
 
 main :: IO ()
 main = do
   let db = initDB
-  let db2 = execState (add2Map 1 bar) db
+  let bar = Set.fromList ['a'..'g'] :: CharSet
+  let baz = Set.fromList ['f'..'m'] :: CharSet
+  let quux = Set.fromList ['n'..'z'] :: CharSet
+
+  let db2 = execState (add2Map 5 bar) db
   let db3 = execState (add2MapsAll baz) db
   -- let db4 = execState (add2MapsSome [1,3] quux) db
+
   print db2
   print db3
-  -- print db4
+  -- pri nt db4
